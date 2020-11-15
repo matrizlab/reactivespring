4079814e3544afb233b14caeeecc148a552200bd
matrizlab <info@matrizlab.com>
2020-11-15 12:39:47 +0100

    start project

---
+++ src/test/java/com/matrizlab/reactivespring/ReactivespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrizlab.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
