bafd2bd7d9e045749d117a47ddcb473f0e03a6c9
matrizlab <info@matrizlab.com>
2020-11-16 17:31:55 +0100

    update controller

--- src/main/java/com/matrizlab/reactivespring/controller/ReservationResource.java
+++ src/main/java/com/matrizlab/reactivespring/controller/ReservationResource.java
@@ -1,6 +1,8 @@
 package com.matrizlab.reactivespring.controller;
 
 import com.matrizlab.reactivespring.model.Reservation;
+import com.matrizlab.reactivespring.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -12,32 +14,39 @@ public class ReservationResource {
 
     public static final String ROOM_V_1_RESERVATION = "/room/v1/reservation/";
 
-    @GetMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> getReservationById(@PathVariable String roomId) {
+    private final ReservationService reservationService;
 
-        //reservationService.getReservation(roomId);
+    @Autowired
+    public ReservationResource(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
 
-        return Mono.just("{}");
+    @GetMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Reservation> getReservationById(@PathVariable String id) {
+
+        return reservationService.getReservation(id);
     }
 
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> createReservation(@RequestBody Mono<Reservation> reservation) {
+    public Mono<Reservation> createReservation(@RequestBody Mono<Reservation> reservation) {
 
-        return Mono.just("{}");
+        return reservationService.createReservation(reservation);
     }
 
-    @PutMapping(path = "{roomId}", produces = MediaType.APPLICATION_JSON_VALUE,
+
+    @PutMapping(path = "{id}", produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<String> updatePrice(@PathVariable String roomId,
-                                    @RequestBody Mono<Reservation> reservation) {
+    public Mono<Reservation> updatePrice(@PathVariable String id,
+                                         @RequestBody Mono<Reservation> reservation) {
 
-        return Mono.just("{}");
+        return reservationService.updateReservation(id, reservation);
     }
 
-    @DeleteMapping(path = "{roomId}")
-    public Mono<Boolean> deleteReservation(@PathVariable String roomId) {
+    @DeleteMapping(path = "{id}")
+    public Mono<Boolean> deleteReservation(@PathVariable String id) {
 
-        return Mono.just(true);
+        return reservationService.deleteReservation(id);
     }
 }
+
