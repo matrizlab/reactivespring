2fa9be2ad2af5849352f34cd6f6b867e04582a98
matrizlab <info@matrizlab.com>
2020-11-16 18:04:42 +0100

    add angular form and service

--- reservation-app/src/app/app.component.ts
+++ reservation-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
-import {HttpClient} from '@angular/common/http';
+import {ReservationService, ReservationRequest, Reservation} from './reservation.service';
+
 
 @Component({
   selector: 'app-root',
@@ -10,21 +11,67 @@ import {HttpClient} from '@angular/common/http';
 export class AppComponent {
   title = 'reservation-app';
 
-  constructor(private http:HttpClient) {}
+  constructor(private reservationService: ReservationService) {}
 
-    private baseUrl:string = 'http://localhost:8080';
-    private reservationUrl:string = this.baseUrl + '/room/v1/reservation/';
+  rooms: Room[];
+  roomSearchForm: FormGroup;
+  currentCheckInVal: string;
+  currentCheckOutVal: string;
+  currentPrice: number;
+  currentRoomNumber: number;
+  currentReservations: Reservation[];
 
-    rooms: Room[];
+  ngOnInit() {
+    this.roomSearchForm = new FormGroup ({
+      checkin: new FormControl(''),
+      checkout: new FormControl(''),
+      roomNumber: new FormControl('')
+    });
 
-      ngOnInit() {
-        this.rooms = [ new Room("127", "127", "150"),
-        new Room("138", "138", "180"),
-        new Room("254", "254", "200")
-        ];
+    this.roomSearchForm.valueChanges.subscribe(form => {
+
+      this.currentCheckInVal = form.checkin;
+      this.currentCheckOutVal = form.checkout;
+
+      if(form.roomNumber) {
+        let roomValues: string[] = form.roomNumber.split('|');
+        this.currentRoomNumber = Number(roomValues[0]);
+        this.currentPrice = Number(roomValues[1]);
       }
 
+      console.log(this.currentCheckInVal);
+      console.log(this.currentCheckOutVal);
+      console.log(this.currentRoomNumber);
+      console.log(this.currentPrice);
+
+    });
+
+    this.rooms = [ new Room("127", "127", "150"),
+    new Room("138", "138", "180"),
+    new Room("254", "254", "200")
+    ];
+
+    this.getCurrentReservations();
+  }
+
+  getCurrentReservations() {
+    this.reservationService.getReservations()
+      .subscribe( getResult => {
+        console.log(getResult);
+        this.currentReservations = getResult;
+      });
+  }
+
+  createReservation() {
+    this.reservationService.createReservation(new ReservationRequest(this.currentRoomNumber,
+    this.currentCheckInVal, this.currentCheckOutVal, this.currentPrice)).subscribe(postResult => {
+      console.log(postResult);
+      this.getCurrentReservations();
+    });
+  }
 }
+
+
 
 export class Room {
   id: string;
@@ -36,4 +83,5 @@ export class Room {
     this.roomNumber = roomNumber;
     this.price = price;
   }
+
 }
