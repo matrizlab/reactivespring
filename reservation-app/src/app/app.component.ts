71921c9725fab80a33c6771db87d41d4001aea46
matrizlab <info@matrizlab.com>
2020-11-16 16:59:03 +0100

    add html

--- reservation-app/src/app/app.component.ts
+++ reservation-app/src/app/app.component.ts
@@ -14,4 +14,26 @@ export class AppComponent {
 
     private baseUrl:string = 'http://localhost:8080';
     private reservationUrl:string = this.baseUrl + '/room/v1/reservation/';
+
+    rooms: Room[];
+
+      ngOnInit() {
+        this.rooms = [ new Room("127", "127", "150"),
+        new Room("138", "138", "180"),
+        new Room("254", "254", "200")
+        ];
+      }
+
+}
+
+export class Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+
+  constructor(id: string, roomNumber: string, price: string) {
+    this.id = id;
+    this.roomNumber = roomNumber;
+    this.price = price;
+  }
 }
